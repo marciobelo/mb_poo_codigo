283507dc8d300ba320ebc046a6603ab9eaa53114
mbelo <y3u89861676>
2019-02-05 22:22:13 -0200

    Exemplos entrada e saída

---
+++ src/main/java/br/nom/belo/marcio/SomaConsole.java
@@ -0,0 +1,17 @@
+package br.nom.belo.marcio;
+
+import java.io.Console;
+
+public class SomaConsole {
+    public static void main(String[] args) 
+    {
+        Console c = System.console();
+        String vs1 = c.readLine();
+        String vs2 = c.readLine();
+        int v1,v2;
+        v1 = Integer.parseInt( vs1 );
+        v2 = Integer.parseInt( vs2 );
+        System.out.println( "resultado da soma " + ( v1 + v2 ) );
+
+    }
+}
