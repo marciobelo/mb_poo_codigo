aa3de26ca70decf5f50ed850e85c0f289af7b3a9
mbelo <y3u89861676>
2019-02-12 23:15:14 -0200

    Exercicio dos divisores

---
+++ src/main/java/br/nom/belo/marcio/DivisoresInteiroOuPrimo.java
@@ -0,0 +1,31 @@
+package br.nom.belo.marcio;
+
+import java.util.Scanner;
+
+public class DivisoresInteiroOuPrimo 
+{
+	public static void main(String[] args) 
+	{		
+		System.out.print( "Digite um inteiro:");
+		Scanner entrada = new Scanner( System.in);
+		int valor = entrada.nextInt();
+		entrada.close();
+		boolean temDivisor = false;
+		for( int possivelDivisor= valor-1; possivelDivisor>1; possivelDivisor--)
+		{
+			if( valor % possivelDivisor == 0)
+			{
+				System.out.println( possivelDivisor);
+				temDivisor = true;
+			}
+		}
+		if( temDivisor)
+		{
+			System.out.println("1");
+		}
+		else
+		{
+			System.out.println( "PRIMO!");
+		}
+	}
+}
